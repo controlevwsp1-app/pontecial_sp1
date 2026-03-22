423bcfff332bc317b7f0f1bbdb90f683bfe087d7
controlevwsp1-app <controlevw.sp1@gmail.com>
2026-03-21 23:33:57 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.CARBANK_CONFIG = {
+  supabaseUrl: 'https://rgutyxnpbucwipfvtybu.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJndXR5eG5wYnVjd2lwZnZ0eWJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxNDE5NTgsImV4cCI6MjA4OTcxNzk1OH0.EGjSCaTN8y8jT9_4mU4jj9TBGNo-JjbjDGXwkmlBBY8',
+};
